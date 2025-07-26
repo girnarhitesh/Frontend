3e07bc91f98abe3039b040acd9d43573bec1c159
girnarhitesh <hiteshgirnar99@gmail.com>
2025-07-26 11:14:54 +0530

    code change

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
         <Navigation />
         <Routes>
           <Route path="/" element={<AllHomepage />} />
-          <Route path='/about' element={<About />} />
+          <Route path="/about" element={<About />} />  {/* Must exist */}
         </Routes>
       </BrowserRouter>
     </>
