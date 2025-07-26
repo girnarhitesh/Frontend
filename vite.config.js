679c9dfa76a19ee1aed614a0b9cac7def34ecf31
girnarhitesh <hiteshgirnar99@gmail.com>
2025-07-26 18:10:11 +0530

    flie code change

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    historyApiFallback: true,
+  build: {
+    outDir: 'dist',
   },
+  server: {
+    historyApiFallback: true, // handles routing fallback in dev
+  }
 });
