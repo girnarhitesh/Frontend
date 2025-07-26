3c076783f34092baa06f0e3faf968f817ea11e6a
girnarhitesh <hiteshgirnar99@gmail.com>
2025-07-26 18:11:56 +0530

    file code change

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-  },
   server: {
-    historyApiFallback: true, // handles routing fallback in dev
-  }
+    historyApiFallback: true,
+  },
 });
